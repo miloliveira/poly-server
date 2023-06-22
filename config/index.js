e5f5b1569d98b3bf171a8010e4a00e2c66a29201
Mariana <marianalucas018@gmail.com>
2023-06-22 10:41:15 +0100

    fixing body parser-error

--- config/index.js
+++ config/index.js
@@ -33,6 +33,6 @@ module.exports = (app) => {
 
   // To have access to `body` property in the request
   app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({ limit: "50mb" }));
+  app.use(express.urlencoded({extended:true}));
   app.use(cookieParser());
 };
