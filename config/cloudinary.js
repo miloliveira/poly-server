cca7086b788427b07eecd4102bab6b522e669724
Mariana <marianalucas018@gmail.com>
2023-12-20 12:04:44 +0000

    Added comments in order to improve code readability to cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,19 +1,25 @@
+// Dependencies
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
+// Configure Cloudinary with API credentials
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Create Cloudinary storage for multer
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
+    // Define allowed formats and folder for uploaded images
     allowed_formats: ["jpg", "img", "jpeg", "gif", "png"],
+    // Specify the folder in which uploaded images will be stored on Cloudinary
     folder: "appcrud",
   },
 });
 
+// Export multer middleware with Cloudinary storage
 module.exports = multer({ storage });
